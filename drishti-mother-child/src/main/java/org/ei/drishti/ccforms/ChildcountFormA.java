52ec1b171b3c60cf52f9ad7a483317f830fe7536
katembu <mose.kate@gmail.com>
2012-08-14 15:27:35 +0300

    added forgoten forms

---
+++ drishti-mother-child/src/main/java/org/ei/drishti/ccforms/ChildcountFormA.java
@@ -0,0 +1,130 @@
+/*
+* PUSHES DATA FROM COMMCARE MODULES TO CC+ REGISTRATION FORM A
+* Main Commcare Modules
+*     == child
+*     == pregnancy
+*     == households
+* 
+* CHILDCOUNT FORM A>Registration DATA to be mapped with commcare modules
+*     Health ID
+*     Location
+*     Full name
+*     Gender
+*     dob
+*     household Health ID
+*  
+*  String register_pattern = "{HID} +NEW {location} {fullname} {gender} {dob} {household_head}";
+* 
+* CHILDCOUNT FORM A>Birth DATA to be mapped with commcare modules
+*     Health ID
+*     Place of Birth
+*     Birth Weight
+*
+*  String birth_pattern = "{HID} +BIR {delivered_inclinic(Y/N/U)} {weight} ";
+*/
+
+package org.ei.drishti.ccforms;
+
+import org.ei.drishti.utils.ChildcountBridge;
+import org.ei.drishti.contract.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.text.MessageFormat;
+
+@Service
+public class ChildcountFormA {
+	
+	private String url;
+	private String chw;
+	private String identity;
+	private String message;
+	private static String params;
+	private static String mob;
+	
+	private static String register_pattern = "{0} +NEW {1} {2} {3} {4} {5}";
+	private static String birth_pattern = " +BIR {0} {1} {2} ";
+	//private String mobile_pattern = "{0} +MOB {1} ";
+	private static String mobile_pattern = " +MOB {0} ";
+	
+	
+	public void registerUnderFive(UnderFiveRegistrationInformation info){
+	
+		    Object[] params = new Object[] {info.healthID(), info.healthID(), info.fullname(), Sex(info.sex()), info.age() ,  info.householdhealthid() };
+		    String message = MessageFormat.format(register_pattern, params);
+		    System.out.println(message);
+		    
+		    //Add BIRTH
+		    String clinicdelivery = info.clinicdelivery();
+		    String motherid = info.motherhealthid();
+		    if(motherid == ""){
+		        motherid = info.householdhealthid();
+		    }
+		    if(clinicdelivery=="yes" || clinicdelivery=="no"){
+		        //Object[] bir = new Object[] {motherid, info.clinicdelivery(), info.birthweight()};
+		        //message += MessageFormat.format(birth_pattern, bir);
+		    }
+		    
+		    ChildcountBridge send = new ChildcountBridge(info.CHWIdentity(), message, "", "");
+		    send.submitPost();
+	}
+
+	public  void registerHousehold(HouseholdRegistrationInformation info){
+			
+		    Object[] params = new Object[] {info.healthID(), info.healthID(), info.fullname(), Sex(info.sex()), info.age() ,  "H" };
+		    String message = MessageFormat.format(register_pattern, params);
+		    System.out.println(message);
+
+		    //ADD MOBILE PHONE
+		    String mobile = info.mobile();
+		    if(mobile !="" )
+		    {
+		        Object[] mob = new Object[] {info.mobile()};
+		        message += MessageFormat.format(mobile_pattern, mob);
+		    }
+		    
+		    ChildcountBridge send = new ChildcountBridge(info.CHWIdentity(), message, "", "");
+		    send.submitPost();
+		    
+	}
+	
+	
+	public void registerPregnancy(PregnancyRegistrationInformation info){
+	
+		    Object[] params = new Object[] {info.healthID(), info.healthID(), info.fullname(), Sex(info.sex()), info.age() ,  info.householdhealthid() };
+		    message = MessageFormat.format(register_pattern, params);
+		    
+		    //ADD MOBILE PHONE
+		    String mobile = info.mobile();
+		    if(mobile !="" )
+		    {
+		        Object[] mob = new Object[] {info.mobile()};
+		        message += MessageFormat.format(mobile_pattern, mob);
+		    }
+		    
+		    System.out.println(message);
+		    ChildcountBridge send = new ChildcountBridge(info.CHWIdentity(), message, "", "");
+		    send.submitPost();
+		    
+		    
+		    
+	}
+		
+
+	private static String Sex(String sex){
+	
+	    if(sex.toLowerCase()=="male"){
+			   return "M";
+		}
+		else if(sex.toLowerCase()=="female"){
+		    return  "F";
+		}
+		else{
+		    return "F";
+		}
+	}
+
+
+}
+
+
+
---
+++ drishti-mother-child/src/main/java/org/ei/drishti/ccforms/ChildcountFormB.java
@@ -0,0 +1,74 @@
+/*
+* PUSHES DATA FROM COMMCARE MODULES TO CC+ Household Visit FORM B
+* Main Commcare Modules
+*     == households visit
+* 
+* CHILDCOUNT FORM B > Registration DATA to be mapped with commcare modules
+*     Health ID
+*     +V 
+*     
+*     household Health ID
+*  
+*  String visit_pattern = "{HID} +V {member_available} {underfive} {advice}";
+*  String fplanning_pattern = "{HID} +P {women} {using_fplanning} {method}";
+* 
+*/
+
+package org.ei.drishti.ccforms;
+
+import org.ei.drishti.utils.ChildcountBridge;
+import org.ei.drishti.contract.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.text.MessageFormat;
+
+@Service
+public class ChildcountFormB {
+	
+
+	private String message;
+	private static String params;
+	
+	private static String visit_pattern = "{0} +V {1} {2} {3}";
+	private static String fplanning_pattern = "{0} +P {1} {2} {3} ";
+	private static String sick_pattern = "{0} +E {1} {2} {3} ";
+	
+	
+	public void householdvisit(HouseholdVisitForm info){
+	
+		    //HOUSEHOLD VISIT (+V)
+		    Object[] params = new Object[] {info.healwthID(), info.member_available(), info.under_five(), info.advice_given() };
+		    String vmessage = MessageFormat.format(visit_pattern, params);
+		    System.out.println(vmessage);
+		    
+		    ChildcountBridge send = new ChildcountBridge(info.CHWIdentity(), vmessage, "", "");
+		    send.submitPost();
+		    
+		    //OTHER SICK MEMBERS SECTION (+E)
+		    //if there sick members and RDT have been used.
+		    /*
+		    String other_sickmember = info.other_sickmembers();
+		    String rdt_used;
+		    
+		    Object[] params = new Object[] {info.healwthID(), info.other_sickmembers(), info.under_five(), info.advice_given() };
+		    String message = MessageFormat.format(sick_pattern, params);
+		    System.out.println(message);
+		    
+		    
+		    ChildcountBridge send = new ChildcountBridge(info.CHWIdentity(), message, "", "");
+		    send.submitPost();
+		    */
+		    //FAMILY PLANNING SECTION (+P)
+		    Object[] params = new Object[] {info.healwthID(), info.women_count(), info.women_using_fplanning(), info.fplanning_method() };
+		    String pmessage = MessageFormat.format(fplanning_pattern, params);
+		    System.out.println(pmessage);
+		    
+		    
+		    ChildcountBridge send = new ChildcountBridge(info.CHWIdentity(), pmessage, "", "");
+		    send.submitPost();
+	}
+
+}
+
+
+
